dc7212285a7638d9e52f08a2b6250436a8964116
Yuri Varvaruk <yvarvaruk@protonmail.com>
2020-10-20 22:18:34 +0300

    loaded executable now clears the screen

--- hello/main.s
+++ hello/main.s
@@ -1,4 +1,8 @@
 org     0x7E00
 bits    32
 
-main:   hlt
+main:   mov     ax, 0x0F20   ; empty VGA entry (white text on black background)
+        mov     edi, 0xB8000 ; VGA buffer linear address
+        mov     ecx, 2000    ; VGA buffer length in entries
+        rep     stosw        ; clear the screen
+.hlt:   hlt
